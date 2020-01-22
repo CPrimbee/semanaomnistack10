3df9a4da40c2b68b8f4cd7509fb6fb5ff8129335
CPrimbee <cprimbee@gmail.com>
2020-01-22 16:53:26 -0300

    Aula 02

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-l8ncc.mongodb.net/week10?retryWrites=true&w=majority',{
-    useNewUrlParser: true,    
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-l8ncc.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
     useUnifiedTopology: true
-});
+  }
+);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
